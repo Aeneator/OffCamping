9ba8169d9c524dc262ff25ffcd9ed1af6855f0ec
Aeneator <58983620+Aeneator@users.noreply.github.com>
2021-10-08 18:25:34 +0300

    Added Navigation

--- App.js
+++ App.js
@@ -1,22 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>If you can see this message then the app is working!</Text>
-      <Text>Pretty cool, right?</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      }
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>If you can see this message then the app is working!</Text>
+      <Text>Pretty cool, right?</Text>
+      {/* <StatusBar style="auto" /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default HomeScreen;
